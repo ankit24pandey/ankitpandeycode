573910f1ccd83a34920c0f321d46ddb96006cec0
ankit24pandey <87513331+ankit24pandey@users.noreply.github.com>
2021-07-19 17:34:12 +0530

    Add files via upload

---
+++ external_builtin_modules.py
@@ -0,0 +1,12 @@
+# TO PRINT RANDOM NUMBER-------------------------------------------------
+
+import random
+randomnumber=random.randint(1,11)
+print(randomnumber)
+
+# TO PRINT RANDOM THING FROM LIST--------------------------------------
+
+l1=["star sports","republic bharat","sony","xyz","ten sports"]
+choice=random.choice(l1)
+print(choice)
+
---
+++ fstrings.py
@@ -0,0 +1,28 @@
+"""import math     # MODULE MATH #
+me="ankit"
+a=24
+x=f"this is {me} {a} {math.cos(0)}"
+print(x)"""
+import random
+l=("snake","water","gun")
+choice=random.choice(l)
+# print(choice)
+print("enter user choice")
+n=input()
+if n==choice:
+    print("draw")
+elif n=="water" and choice=="snake":
+    print("computer win")
+elif n=="snake" and choice=="water":
+    print("you win")
+elif n=="gun" and choice=="snake":
+    print("you win")
+elif n=="snake" and choice=="gun":
+    print("computer win")
+elif n=="water" and choice=="gun":
+    print("computer win")
+elif n=="gun" and choice=="water":
+    print("you win")
+else:
+    print("error")
+print("computer choice is",choice)
