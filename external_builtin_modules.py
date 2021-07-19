ab7bfd64121d3eba04450e24345bc9435001a410
ankit24pandey <87513331+ankit24pandey@users.noreply.github.com>
2021-07-19 17:46:14 +0530

    Update external_builtin_modules.py

--- external_builtin_modules.py
+++ external_builtin_modules.py
@@ -9,4 +9,28 @@ print(randomnumber)
 l1=["star sports","republic bharat","sony","xyz","ten sports"]
 choice=random.choice(l1)
 print(choice)
+# FUN GAME--- GIVE INPUT ANYTHING FROM(WATER/GUN/SNAKE) AND COMPUTER ALSO PRINT SOMETHING RANDOM FROM(WATER/GUN/SNAKE)-----------------------
+import random
+l=("snake","water","gun")
+choice=random.choice(l)
+# print(choice)
+print("enter user choice")
+n=input()
+if n==choice:
+    print("draw")
+elif n=="water" and choice=="snake":
+    print("computer win")
+elif n=="snake" and choice=="water":
+    print("you win")
+elif n=="gun" and choice=="snake":
+    print("you win")
+elif n=="snake" and choice=="gun":
+    print("computer win")
+elif n=="water" and choice=="gun":
+    print("computer win")
+elif n=="gun" and choice=="water":
+    print("you win")
+else:
+    print("error")
+print("computer choice is",choice)
 
